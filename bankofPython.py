83969919c8583de9c1291c036dd1489da7f493cf
SamedliSamuray <samedlisamuray@gmail.com>
2024-05-13 00:16:02 +0400

    update

--- bankofPython.py
+++ bankofPython.py
@@ -1,34 +1,65 @@
-class addAccount():
-    def __init__(self,accountNumbers,password):
-        self.accountNumbers=accountNumbers
-        self.password=password
-        self.money=0
-    def aboutAccount(self):       
-        print(f'Əziz istifadəçi sizin hesabinizda {self.money} Azn pul var.')   
-    def addMoney(self,money):
-        self.money+=money
-        print(f'Əziz istifadəçi sizin hesabiniza {money} Azn elave olundu.Toplam mebleg {self.money}')  
-    def withdraw(self,money):
-        self.money-=money
-        print(f'Əziz istifadəçi sizin hesabinizdan {money} Azn çıxarıldı.Toplam mebleg {self.money}') 
-        
-class Credits(addAccount):
-    def __init__(self,accountNumbers, password):
-        super().__init__(accountNumbers,password)
-    def getLoan(self,amount):
-        self.loan=amount
-        monthMoney=amount/12
-        print(f'Istediyiniz {amount} azn mebleg verildi. Ayliq {monthMoney} azn odemeli olacaqsiniz')
-    def payLoan(self,a):
-        self.loan-=a
-        print(f'Lazım olan ödənişdən artıq olan {int(-self.loan)} azn məbləğ geri qaytarıldı. Borcunuz : 0 azn') if self.loan<=0 else  print(f'Ödədiyiniz {a} azn məbləğ kreditdən çıxıldı . Qalıq borc : {self.loan}')
-        
-user1=addAccount(3640817598474185,1903)
-user1.aboutAccount()
-user1.addMoney(750)
-user1.addMoney(320)
+class AddAccount:
+    def __init__(self,name,surname,account_number, password):
+        self.money = 0
+        self.name = name
+        self.surname = surname
+        self.account_number = account_number
+        self.password = password
+
+    def about_account(self):
+        print(f'{self.name} {self.surname} , hesabınızda {self.money} AZN pul var')
+
+    def add_money(self, money):
+        self.money += money
+        print(f'{self.name} {self.surname} ,hesabınıza {money} AZN əlavə olundu. Toplam balans :  {self.money} AZN')
+
+    def withdraw(self, money):
+        if money <= self.money:
+            self.money -= money
+            print(f'{self.name} {self.surname} ,hesabınızdan {money} AZN pul çəkildi.. Toplam balans: {self.money} AZN')
+        else:
+            print("Yetərsiz vəsait. Əməliyyat ləğv edildi.")
+
+class Credits:
+    def __init__(self):
+        self.accounts = []
+
+    def add_account(self, account):
+        self.accounts.append(account)
+        print("Hesab uğurla əlavə edildi.")
+
+    def get_loan(self, account, amount):
+        account.add_money(amount)
+        self.creditMoney=0
+        self.moneyMonth=amount/12
+        self.creditMoney+=amount
+        print(f"{account.name} {account.name} ,siz {amount} AZN məbləğində kredit götürmüsünüz.Ayliq odenis minimal olaraq {self.moneyMonth} Azn. İndi ümumi balansınız: {account.money} AZN")
+
+    def pay_loan(self, account, amount):
+        if amount <= account.money:
+            account.withdraw(amount)
+            self.creditMoney-=amount
+            print(f"{account.name} {account.surname} ,siz kreditinizin {amount} AZN-ni ödəmisiniz. Qalan kredit borcunuz: {self.creditMoney} AZN. Balansiniz {account.money} AZN")
+        else:
+            print("Yetərsiz vəsait. Əməliyyat ləğv edildi.")
+
+user1 = AddAccount('Samuray','Samedli',3640817598474185, 1903)
+
+user1.about_account()
+user1.add_money(750)
+user1.add_money(320)
 user1.withdraw(457)
-kredit=Credits(1212343456567878,1999)
-kredit.getLoan(1200)
-kredit.payLoan(200)
-kredit.payLoan(1500)
+
+credit = Credits()
+credit.add_account(user1)
+credit.get_loan(user1, 1200)
+credit.pay_loan(user1, 200)
+
+user2 = AddAccount('Necip','Uysal',1111111111111111, 1999)
+user2.about_account()
+user2.add_money(1340)
+user2.add_money(100)
+user2.withdraw(800)
+credit.add_account(user2)
+credit.get_loan(user2, 2400)
+credit.pay_loan(user2, 200)
